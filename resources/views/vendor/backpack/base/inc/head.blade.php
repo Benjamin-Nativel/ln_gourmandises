0d473b28e61fd9755912476651f22a4837e9a361
Benjamin-Nativel <benjaminhenri.nativel31@gmail.com>
2022-10-04 08:06:13 +0200

    fix button back

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 @vite('resources/css/app.css')
 @if (config('backpack.base.meta_robots_content'))
     <meta name="robots" content="{{ config('backpack.base.meta_robots_content', 'noindex, nofollow') }}">
